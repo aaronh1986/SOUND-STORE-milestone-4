ba28fcb4ef40661ccc552310998bd8340957a0b8
aaronh1986 <aaronharbourne@gmail.com>
2021-10-11 16:38:18 +0000

    Add signals to checkout app

--- checkout/admin.py
+++ checkout/admin.py
@@ -2,7 +2,13 @@ from django.contrib import admin
 from .models import Order, OrderLineItem
 
 
+class OrderLineItemAdminInline(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
 class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderLineItemAdminInline,)
+
     readonly_fields = ('order_number', 'date',
                        'delivery_cost', 'order_total',
                        'grand_total',)
@@ -18,3 +24,6 @@ class OrderAdmin(admin.ModelAdmin):
                     'grand_total',)
 
     ordering = ('-date',)
+
+
+admin.site.register(Order, OrderAdmin)
