cd89978961815b34207fe63b252535145b0ff70c
aaronh1986 <aaronharbourne@gmail.com>
2021-10-11 18:03:14 +0000

    Setup checkout.html file

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
-# Create your views here.
+from .forms import OrderForm
+
+
+def checkout(request):
+    bag = request.session.get('bag', {})
+    messages.error(request, "Cart empty for now")
+    return redirect(reverse('products'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
