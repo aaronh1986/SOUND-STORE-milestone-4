d791cc9f5c5945d2c03032bf024a46d4832e280e
aaronh1986 <aaronharbourne@gmail.com>
2021-10-11 19:45:21 +0000

    Finalise checkout form for now

--- checkout/views.py
+++ checkout/views.py
@@ -6,8 +6,9 @@ from .forms import OrderForm
 
 def checkout(request):
     bag = request.session.get('bag', {})
-    messages.error(request, "Cart empty for now")
-    return redirect(reverse('products'))
+    if not bag:
+        messages.error(request, "Cart empty for now")
+        return redirect(reverse('products'))
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
@@ -16,3 +17,4 @@ def checkout(request):
     }
 
     return render(request, template, context)
+
