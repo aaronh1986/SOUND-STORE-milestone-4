5b12bc3a45a91dbdd1763dfe8f3261eb4be75395
aaronh1986 <aaronharbourne@gmail.com>
2021-10-12 21:28:18 +0000

    Add webhoks.py with method to connect to stripe..test using a print statement

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -7,9 +7,26 @@ class StripeWH_Handler:
     def __init__(self, request):
         self.request = request
 
-    
     def handle_event(self, event):
-        """Handle an unknown or unexpected webhook event"""
+        """Handle an unknown or unexpected or generic webhook event"""
+        return HttpResponse(
+            content=f'Unhandled {event["type"]} Webhook was received',
+            status=200,)
+
+    def handle_payment_intent_succeeded(self, event):
+        """
+        Handle payment_intent.succeeded webhook from Stripe
+        """
         return HttpResponse(
             content=f'{event["type"]} Webhook was received',
             status=200,)
+
+    def handle_payment_intent_payment_failed(self, event):
+        """
+        Handle payment_intent.payment_failed webhook from Stripe
+        """
+        return HttpResponse(
+            content=f'{event["type"]} Webhook was received',
+            status=200,)
+
+    
