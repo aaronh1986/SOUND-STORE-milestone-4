9f7ed642b3788a77120d07117923c5385e91d025
aaronh1986 <aaronharbourne@gmail.com>
2021-10-13 11:20:48 +0000

    Add payment caching in payment intent

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -17,6 +17,8 @@ class StripeWH_Handler:
         """
         Handle payment_intent.succeeded webhook from Stripe
         """
+        intent = event.data.object
+        print(intent)
         return HttpResponse(
             content=f'{event["type"]} Webhook was received',
             status=200,)
