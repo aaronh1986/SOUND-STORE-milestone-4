66c09a1da4f4ac34989a62d5244f4e6d9942ee3e
aaronh1986 <aaronharbourne@gmail.com>
2021-10-18 15:13:29 +0000

    Replace exposed secret key in settings.py

--- sound_store/settings.py
+++ sound_store/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9(ngepb23($q0kusofsi99^o=9s94=vwg20(%+axagw)i5w6vp'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     'ms-4-sound-store.herokuapp.com', 'localhost'
