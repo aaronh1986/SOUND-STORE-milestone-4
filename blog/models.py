3d40e54543b3ef779630a04abf43f255968bb8f9
aaronh1986 <aaronharbourne@gmail.com>
2021-10-21 11:30:04 +0000

    Update procfile to test if it will fix error screen on blog in admin

--- blog/models.py
+++ blog/models.py
@@ -3,11 +3,10 @@ from django.contrib.auth.models import User
 
 
 class BlogPost(models.Model):
-    blog_title = models.CharField(max_length=250, null=False, blank=False)
-    blog_author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name="blog_post")
-    blog_slug = models.SlugField(max_length=200)
-    blog_heading = models.CharField(max_length=400, null=False, blank=False)
-    blog_main = models.TextField()
-    blog_created = models.DateTimeField(auto_now_add=True)
+    blog_title = models.CharField(max_length=250, null=False, blank=False, default='')
+    blog_author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name="blog_post", default='')
+    blog_slug = models.SlugField(max_length=200, default='')
+    blog_heading = models.CharField(max_length=400, null=False, blank=False, default='')
+    blog_main = models.TextField(default='')
     blog_image_url = models.URLField(max_length=1000, null=True, blank=True)
     blog_image = models.ImageField(null=True, blank=True)
